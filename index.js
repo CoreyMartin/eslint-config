4aa5980e6d31c37d9bfbe2ce70af67c3c61c445c
Corey Martin <coreyn.martin@gmail.com>
2018-11-04 16:52:15 -0600

    Removes some overrides that were set to warning due to issues from another codebase

--- index.js
+++ index.js
@@ -16,67 +16,6 @@ module.exports = {
     }
   },
   "rules": {
-    /* temporarily setting these airbnb rules to warn while we work on fixing them */
-    "import/prefer-default-export": "warn",
-    // see https://github.com/airbnb/javascript/issues/1089#issuecomment-249943774 for explanation on this:
-    "react/forbid-prop-types": ["warn", { "forbid": ["any", "array", "object"] }],
-    "react/require-default-props": "warn",
-    "react/no-unused-prop-types": ["warn", {
-      "customValidators": [
-      ],
-      "skipShapeProps": true,
-    }],
-    "no-shadow": "warn",
-    "consistent-return": "error",
-    "class-methods-use-this": ["warn", {
-      exceptMethods: [
-        "render",
-        "getInitialState",
-        "getDefaultProps",
-        "getChildContext",
-        "componentWillMount",
-        "componentDidMount",
-        "componentWillReceiveProps",
-        "shouldComponentUpdate",
-        "componentWillUpdate",
-        "componentDidUpdate",
-        "componentWillUnmount",
-        "componentDidCatch",
-      ],
-    }],
-    "jsx-a11y/anchor-is-valid": ["warn", {
-      components: ["Link"],
-      specialLink: ["to"],
-      aspects: ["noHref", "invalidHref", "preferButton"],
-    }],
-    "no-use-before-define": ["warn", { functions: true, classes: true, variables: true }],
-    "react/no-array-index-key": "warn",
-    "no-param-reassign": ["warn", {
-      props: true,
-      ignorePropertyModificationsFor: [
-        "acc", // for reduce accumulators
-        "e", // for e.returnvalue
-        "ctx", // for Koa routing
-        "req", // for Express requests
-        "request", // for Express requests
-        "res", // for Express responses
-        "response", // for Express responses
-        "$scope", // for Angular 1 scopes
-      ]
-    }],
-    "no-nested-ternary": "warn",
-    "jsx-a11y/click-events-have-key-events": "warn",
-    "jsx-a11y/no-static-element-interactions": ["warn", {
-      handlers: [
-        "onClick",
-        "onMouseDown",
-        "onMouseUp",
-        "onKeyPress",
-        "onKeyDown",
-        "onKeyUp",
-      ]
-    }],
-
     /* airbnb rules that are purposefully overridden: */
     "no-console": [2, {"allow": ["warn", "error"]}],
     "semi": [2, "never"],
